ceed7a718c9522a88911573f6a2452b3f2dccab4
James Hazeldine <hazeldinejames2@gmail.com>
2024-07-14 17:47:25 +1000

    Added user input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::io;
+
 struct Entry {
     datetime_string: String,
     text: String,
 }
 
 impl Entry {
+    fn new(text: String) -> Self {
+        Self {
+            datetime_string: String::from("2024-07-14 11:30:00"),
+            text,
+        }
+    }
+
     fn print_entry(&self) {
         println!("{} - {}", self.datetime_string, self.text);
     }
@@ -12,10 +21,36 @@ impl Entry {
 fn main() {
     println!("~ Journal ~");
 
-    let entry = Entry {
-        datetime_string: String::from("2024-07-14 11:30:00"),
-        text: String::from("This is my first entry"),
-    };
+    loop {
+        let mut choice = String::new();
 
-    entry.print_entry();
+        println!("Main Menu");
+        println!("1. Write New Entry.");
+        println!("2. Quit.");
+        println!("Enter Choice:");
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line.");
+
+        match choice.trim() {
+            "1" => {
+                let mut entry_text = String::new();
+                println!("Type entry and press enter to submit:");
+                io::stdin()
+                    .read_line(&mut entry_text)
+                    .expect("Failed to read line.");
+                let entry_text = entry_text.trim().to_string();
+                let entry = Entry::new(entry_text);
+                entry.print_entry();
+            }
+            "2" => {
+                println!("Exiting Program...");
+                break;
+            }
+            other => {
+                println!("Invalid choice {}. Please try again.", other);
+            }
+        }
+    }
 }
